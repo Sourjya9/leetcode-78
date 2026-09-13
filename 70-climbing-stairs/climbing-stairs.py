88093fadaedee04c564947b1e9bc0cc6048ef0f3
Sourjya9 <banerjeesourjya51@gmail.com>
2026-09-14 02:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n==1:
-            return 1
         a,b=1,2
-        for _ in range(3,n+1):
+        for _ in range(n-1):
             a,b=b,a+b
-
-        return b
+        return a
